ba78bf217d23bc651d69820e9f4252e0c72e9f90
isaklovold <isakloevold@gmail.com>
2016-04-20 12:26:05 +0100

    Implementet interface into bonks and zaps and created GridWorld class

--- Bonks-And-Zaps/src/Zaps.java
+++ Bonks-And-Zaps/src/Zaps.java
@@ -1,4 +1,32 @@
 
-public class Zaps {
+public class Zaps implements Being {
+
+	public Zaps(){
+		
+	}
+	
+	@Override
+	public String getName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void act() throws CannotActException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Position getLocation() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setLocation(Position location) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
